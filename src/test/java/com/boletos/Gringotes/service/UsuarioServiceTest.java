6c0ec367d70ffab27439d4e199bec2a58b3685c1
rhanielfarias <rhanielfarias@gmail.com>
2022-09-22 18:47:15 -0300

    Aplicando teste unitário do método cadastrar do usuário

--- src/test/java/com/boletos/Gringotes/service/UsuarioServiceTest.java
+++ src/test/java/com/boletos/Gringotes/service/UsuarioServiceTest.java
@@ -1,10 +1,12 @@
 package com.boletos.Gringotes.service;
 
+import com.boletos.Gringotes.model.UsuarioModel;
 import com.boletos.Gringotes.model.dto.UsuarioDto;
 import com.boletos.Gringotes.repository.UsuarioRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -24,10 +26,13 @@ class UsuarioServiceTest {
 
     private UsuarioDto usuarioDto;
 
+    private UsuarioModel usuarioModel;
+
     @BeforeEach
     private void inicializador() {
         MockitoAnnotations.openMocks(this);
         usuarioDto = new UsuarioDto(1, "Rhaniel Farias", LocalDate.now(), "rhanielfarias@gmail.com");
+        usuarioModel = new UsuarioModel(1, "Rhaniel Farias", LocalDate.now(), "rhanielfarias@gmail.com", "022.850.650-60");
     }
 
     @Test
@@ -36,5 +41,11 @@ class UsuarioServiceTest {
         assertTrue(todos.isEmpty());
     }
 
+    @Test
+    void verificandoSeEstaSendoEfetuadoOCadastroDoUsuarioComIdNomeDataDeNascimentoEmailECpf() {
+        Mockito.when(usuarioRepository.existsById(Mockito.anyInt())).thenReturn(true);
+        usuarioRepository.save(usuarioModel);
+        Mockito.verify(usuarioRepository, Mockito.times(1)).save(usuarioModel);
+    }
 
 }
