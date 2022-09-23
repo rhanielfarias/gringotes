04400989d22dc40b437bf3fce1262d23d6c157f3
rhanielfarias <rhanielfarias@gmail.com>
2022-09-23 15:27:09 -0300

    Testes unitários de cadastro

--- src/test/java/com/boletos/Gringotes/service/UsuarioServiceTest.java
+++ src/test/java/com/boletos/Gringotes/service/UsuarioServiceTest.java
@@ -3,18 +3,20 @@ package com.boletos.Gringotes.service;
 import com.boletos.Gringotes.model.UsuarioModel;
 import com.boletos.Gringotes.model.dto.UsuarioDto;
 import com.boletos.Gringotes.repository.UsuarioRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -30,27 +32,40 @@ class UsuarioServiceTest {
 
     @BeforeEach
     private void inicializador() {
-        MockitoAnnotations.openMocks(this);
         usuarioDto = new UsuarioDto(1, "Rhaniel Farias", LocalDate.now(), "rhanielfarias@gmail.com");
         usuarioModel = new UsuarioModel(1, "Rhaniel Farias", LocalDate.now(), "rhanielfarias@gmail.com", "022.850.650-60");
     }
 
     @Test
     void exibindoOsUsuariosCasoAListaEstejaVazia() {
-        Mockito.when(usuarioRepository.findAll()).thenReturn(Collections.emptyList());
+        when(usuarioRepository.findAll()).thenReturn(Collections.emptyList());
         List<UsuarioDto> todosOsUsuarios = usuarioService.buscarPorTodos();
-        Assertions.assertTrue(todosOsUsuarios.isEmpty());
+        assertTrue(todosOsUsuarios.isEmpty());
+    }
+
+    @Test
+    void exibindoUsuariosCasoExiste() {
+        when(usuarioRepository.findAll()).thenReturn(List.of(usuarioModel));
+        List<UsuarioDto> todosOsUsuarios = usuarioService.buscarPorTodos();
+        assertFalse(todosOsUsuarios.isEmpty());
     }
 
     @Test
     void verificandoSeEstaSendoEfetuadoOCadastroDoUsuarioComIdNomeDataDeNascimentoEmailECpf() {
-        Mockito.when(usuarioRepository.existsById(Mockito.anyInt())).thenReturn(true);
-        usuarioRepository.save(usuarioModel);
-        Mockito.verify(usuarioRepository, Mockito.times(1)).save(usuarioModel);
+        when(usuarioRepository.existsById(anyInt())).thenReturn(false);
+        usuarioService.cadastrar(usuarioModel);
+        verify(usuarioRepository, times((1))).save(usuarioModel);
     }
 
+
     @Test
-    void verificandoSeOAlterarDoUsuarioEstaFuncionando() {
+    void apresentandoErroCasoExistaUsuarioJaCadastrado() {
+        when(usuarioRepository.existsById(1)).thenReturn(true);
+        assertThrows(RuntimeException.class, () -> {
+            usuarioService.cadastrar(usuarioModel);
+        });
+        verify(usuarioRepository, times(1)).existsById(1);
+
     }
-rrrm
+
 }
