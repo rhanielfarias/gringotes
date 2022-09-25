3871bfebd49e02d15475f3978feb079db5d6068f
rhanielfarias <rhanielfarias@gmail.com>
2022-09-25 18:50:36 -0300

    Criando a teste de modelo

--- src/main/java/com/boletos/Gringotes/model/TesteModel.java
+++ src/main/java/com/boletos/Gringotes/model/TesteModel.java
@@ -1,4 +1,14 @@
 package com.boletos.Gringotes.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class TesteModel {
 }
